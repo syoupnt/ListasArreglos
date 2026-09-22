a48eb78e4f5b3a2921caffa752007f35dba2fd14
Carlos <matiasdezela@gmail.com>
2026-09-22 09:07:23 -0500

    Primera versión

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("asd");
+﻿int[] notas = { 18, 20, 15, 12 };
+
+for (int i = 0; i < notas.Length; i++) {
+    Console.WriteLine($"Nota #{i+1}: {notas[i]}");
+}
--- c#/obj/Debug/net10.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net10.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab1f95a0d5a639661e31795682b8d0f086ed2bd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

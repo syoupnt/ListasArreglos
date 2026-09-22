5430d12dffe180933e45ba181e1a7aa8d70c9dfc
Carlos <matiasdezela@gmail.com>
2026-09-22 09:08:18 -0500

    Primera versión en python

---
+++ README.md
@@ -0,0 +1,4 @@
+# Listas y Arreglos
+
+Proyecto para aprender sobre listas y arreglos
+en python y c#
---
+++ python/main.py
@@ -0,0 +1,4 @@
+notas = [18, 20, 15, 12]
+
+for i in range(len(notas)):
+    print(f'Nota #{i}: {notas[i]}')
